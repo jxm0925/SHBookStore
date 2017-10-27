89d8fed912d9cd6b966890c2e3bbb014c4b759a0
jxm0925 <2922636335@qq.com>
2017-10-27 11:22:28 +0800

    auth 基类构造函数

--- application/admin/controller/Auth.php
+++ application/admin/controller/Auth.php
@@ -3,16 +3,26 @@ namespace app\admin\controller;
 
 use think\Controller;
 use \think\Session;
+use app\admin\model\User;
 use app\admin\model\Admin;
+use app\admin\model\Notice;
+use app\admin\model\Comment;
+use app\admin\model\refund;
 
 class Auth extends Controller
 {
 	protected $is_login = [''];
+	protected $user;
+	protected $notice;
+	protected $comment;
 	public function _initialize()
 	{
 		if (!$this->checklogin() && in_array('*', $this->is_login)) {
 			$this->error('没有登录请登录', url('admin/auth/login'));
 		}
+		$this->user = new User();
+		$this->notice = new Notice();
+		$this->comment= new Comment();
 	}
 	public function login()
 	{
